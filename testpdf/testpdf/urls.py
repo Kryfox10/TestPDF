621ddba5451f68ddeabd7a342a35143b01cee727
Mon <kryfox10@gmail.com>
2013-10-27 23:19:59 +0800

    Hello World PDF

--- testpdf/testpdf/urls.py
+++ testpdf/testpdf/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'testpdf.views.home', name='home'),
+    url(r'^$', 'generatepdf.views.some_view', name='home'),
     # url(r'^testpdf/', include('testpdf.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
